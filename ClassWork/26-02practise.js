5a93cd5b6093231e4dc43bd9d05fd7dc96c4a61f
Ivan Berk <berkoivan@gmail.com>
2020-02-28 22:37:14 +0200

    corrected a method of calculating accommodation in practise 26-02

--- ClassWork/26-02practise.js
+++ ClassWork/26-02practise.js
@@ -34,6 +34,15 @@ class Guest {
       }));
   }
 
+  static getDurationUserVisits(guestArray) {
+    return guestArray
+      .map((user) => ({
+        fullName: `${user.firstName} ${user.lastName}`,
+        quantityDays: user.calcDuration(),
+      }))
+      .sort((prev, next) => next.quantityDays - prev.quantityDays)
+      .filter((user, idx, arr) => arr[0].quantityDays === user.quantityDays);
+  }
   constructor(firstName, lastName, birthDate, gender, isMarriage, room) {
     this.firstName = firstName;
     this.lastName = lastName;
@@ -47,14 +56,19 @@ class Guest {
     this.children = [];
   }
   calcDuration() {
-    return this.visitsHistory.reduce((result, visit) => {
-      result += Date.parse(visit.endDate) - Date.parse(visit.startDate);
-      return result / (1000 * 60 * 60 * 24);
-    }, 0);
+    return (
+      this.visitsHistory.reduce((result, visit) => {
+        result += Date.parse(visit.endDate) - Date.parse(visit.startDate);
+        return result;
+      }, 0) / 86400000
+    );
   }
   calculateCostOfLiving(room) {
     const duration = this.calcDuration();
     this.costOfLiving = duration * costOfLiving[room];
+    if (this.discountLevel) {
+      this.costOfLiving -= (this.costOfLiving * this.discountLevel) / 100;
+    }
   }
   calcDiscount() {
     const duration = this.calcDuration();
@@ -71,7 +85,6 @@ class Guest {
     this.children = [...this.children, new Child(name, age)];
   }
 }
-
 class Child {
   constructor(name, age) {
     this.name = name;
@@ -203,7 +216,57 @@ const guests = [
   },
 ];
 
-// console.log(Guest.getUserByGender(guests, "male"));
+const ourGuests = [];
+
+const guest1 = new Guest(
+  'Ivan',
+  'Berko',
+  '1998-02-02',
+  'male',
+  'true',
+  'superior',
+);
+
+guest1.visitsHistory = [
+  {
+    startDate: '1996-07-03',
+    endDate: '1996-07-05',
+  },
+  {
+    startDate: '1998-11-23',
+    endDate: '1998-11-25',
+  },
+];
+guest1.calcDiscount();
+guest1.calculateCostOfLiving('superior');
+console.log(guest1);
+
+const guest2 = new Guest(
+  'fsas',
+  'asd',
+  '1998-02-02',
+  'gender',
+  'isMarriage',
+  'standart',
+);
+
+guest2.visitsHistory = [
+  {
+    startDate: '1993-06-03',
+    endDate: '1994-08-09',
+  },
+  {
+    startDate: '1998-11-23',
+    endDate: '1998-12-30',
+  },
+];
+
+ourGuests.push(guest2);
+ourGuests.push(guest1);
+console.log(ourGuests);
+
+console.log(Guest.getDurationUserVisits(ourGuests));
+
 const getDateString = (date) =>
   `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
 
@@ -242,13 +305,8 @@ function createUser() {
   }
   newGuest.calculateCostOfLiving(room);
   newGuest.calcDiscount();
-
-  if (newGuest.discountLevel) {
-    newGuest.costOfLiving +=
-      (newGuest.costOfLiving * newGuest.discountLevel) / 100;
-  }
   return newGuest;
 }
 
-const ourGuest = createUser();
-console.dir(ourGuest);
+// const ourGuest = createUser();
+// console.dir(ourGuest);
---
+++ ClassWork/28-02/28-02.js
@@ -0,0 +1,13 @@
+/*
+1. Узлы, что это такое, в чем отличие между элементами?
+2. dataset
+*/
+
+const ul = document.querySelector('ul');
+const childs = ul.children;
+const firstChild = ul.firstElementChild;
+
+const table = document.querySelector('table');
+const rows = table.rows;
+const li = ul.querySelectorAll('li');
+const ulById = document.getElementById('list');

94c2a9566f96131ee0469fb7283faee19d0c0e64
Ivan Berk <berkoivan@gmail.com>
2020-03-20 22:37:41 +0200

    task pictures-filter done

--- HomeWork/pictures-filter/index.js
+++ HomeWork/pictures-filter/index.js
@@ -1,1 +1,16 @@
-console.log(debounce);
+const container = document.querySelector('.container');
+
+container.addEventListener('mouseover', _.debounce(handelMouseover, 300));
+container.addEventListener('mouseout', handelMouseout);
+
+function handelMouseover(event) {
+  if (event.target.className === 'box__img') {
+    event.target.classList.add('remove-filter');
+  }
+}
+
+function handelMouseout(event) {
+  if (event.target.className === 'box__img remove-filter') {
+    event.target.classList.remove('remove-filter');
+  }
+}
